bfe53f97c6e5b8e045f75847de960abf3740b779
ltlaitoff <ltlaitoff@gmail.com>
2023-01-07 13:44:43 +0200

    feat: Added Getting Started View to router

--- vue/vue-study/src/router/index.js
+++ vue/vue-study/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import HomeView from '../views/HomeView.vue'
+import GettingStartedView from '../views/GettingStartedView.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
 			path: '/',
 			name: 'home',
 			component: HomeView
+		},
+		{
+			path: '/getting-started',
+			name: 'getting-started',
+			component: GettingStartedView
 		}
 	]
 })
