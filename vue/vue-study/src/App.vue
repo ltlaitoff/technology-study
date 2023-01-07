f5bb8f00411f5fa4097b2433d1f917ddc849c28b
ltlaitoff <ltlaitoff@gmail.com>
2023-01-07 13:45:07 +0200

    feat: Added router-link to GettingStarted View

--- vue/vue-study/src/App.vue
+++ vue/vue-study/src/App.vue
@@ -9,7 +9,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 			<nav>
 				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/about">About</RouterLink>
+				<RouterLink to="/getting-started">Getting started</RouterLink>
 			</nav>
 		</div>
 	</header>
