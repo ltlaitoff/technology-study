c6bcc3a93c69f132a7bb2c4a9d2e85f4c6ebacfa
ltlaitoff <ltlaitoff@gmail.com>
2023-01-07 13:53:59 +0200

    chore: Created tailwind config

---
+++ vue/vue-study/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// @ts-ignore CommonJS export
+// eslint-disable-next-line
+module.exports = {
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		extend: {}
+	},
+	plugins: []
+}
