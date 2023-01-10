41e974287929f5b20459b55c7c0ab98aa009b58a
ltlaitoff <ltlaitoff@gmail.com>
2023-01-10 18:16:28 +0200

    style: Added styles to App and GettingStartedView

--- vue/vue-study/src/App.vue
+++ vue/vue-study/src/App.vue
@@ -1,15 +1,25 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from 'vue-router'
+import { routes } from './router'
+
+const route = useRoute()
 </script>
 
 <template>
 	<header>
-		<div class="wrapper">
-			<HelloWorld msg="You did it!" />
-
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/getting-started">Getting started</RouterLink>
+		<div class="mb-4">
+			<nav class="flex gap-4">
+				<RouterLink
+					:class="[
+						`text-sky-500 hover:text-sky-600 transition `,
+						{ 'text-amber-400': route.path === currentRoute.path }
+					]"
+					v-for="currentRoute in routes"
+					:key="currentRoute.path"
+					:to="currentRoute.path"
+				>
+					{{ currentRoute.name }}
+				</RouterLink>
 			</nav>
 		</div>
 	</header>
@@ -17,66 +27,4 @@ import { RouterLink, RouterView } from 'vue-router'
 	<RouterView />
 </template>
 
-<style scoped>
-header {
-	line-height: 1.5;
-	max-height: 100vh;
-}
-
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
-nav {
-	width: 100%;
-	font-size: 12px;
-	text-align: center;
-	margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-	color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-	background-color: transparent;
-}
-
-nav a {
-	display: inline-block;
-	padding: 0 1rem;
-	border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-	border: 0;
-}
-
-@media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
-
-	nav {
-		text-align: left;
-		margin-left: -1rem;
-		font-size: 1rem;
-
-		padding: 1rem 0;
-		margin-top: 1rem;
-	}
-}
-</style>
+<style></style>
--- vue/vue-study/src/views/GettingStartedView.vue
+++ vue/vue-study/src/views/GettingStartedView.vue
@@ -1,7 +1,20 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+
+/* */
+</script>
 
 <template>
-	<div>GettingStartedView</div>
+	<div class="font-base">
+		Introduction example:
+		<button
+			class="rounded bg-zinc-100 px-4 py-2 text-slate-700 border-2 border-slate-200 hover:bg-zinc-200 hover:border-slate-300 transition"
+			@click="count++"
+		>
+			Count is: {{ count }}
+		</button>
+	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style></style>
