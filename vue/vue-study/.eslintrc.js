0b6eeff826eee7db2a07f5508b79ffec6e432346
ltlaitoff <ltlaitoff@gmail.com>
2023-01-10 09:32:01 +0200

    refactor: Rename eslint config from cjs to js

--- vue/vue-study/.eslintrc.js
+++ vue/vue-study/.eslintrc.js
@@ -1,10 +1,13 @@
-/* eslint-env node */
+/* eslint-env es2020, node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
 	root: true,
 	extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
 	parserOptions: {
-		ecmaVersion: 'latest'
+		ecmaVersion: 2022
+	},
+	rules: {
+		'no-debugger': 'off'
 	}
 }
