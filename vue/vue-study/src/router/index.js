d1ab27e7282cf3a7d5d656a937335d47867785ee
ltlaitoff <ltlaitoff@gmail.com>
2023-01-10 18:15:35 +0200

    feat: Moved routes to var and export

--- vue/vue-study/src/router/index.js
+++ vue/vue-study/src/router/index.js
@@ -3,20 +3,24 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GettingStartedView from '../views/GettingStartedView.vue'
 
+const routes = [
+	{
+		path: '/',
+		name: 'Home',
+		component: HomeView
+	},
+	{
+		path: '/getting-started',
+		name: 'Getting Started',
+		component: GettingStartedView
+	}
+]
+
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: HomeView
-		},
-		{
-			path: '/getting-started',
-			name: 'getting-started',
-			component: GettingStartedView
-		}
-	]
+	routes: routes
 })
 
 export default router
+
+export { routes }
